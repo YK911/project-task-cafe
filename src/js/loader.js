b857e2d9bc6519a4e1c404391f767bd203508cde
Mariia Illarionova <illarionovam@gmail.com>
2024-07-06 13:51:18 +0300

    loader starts at least after 0.5 second

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,7 +1,15 @@
-export function showLoader() {
+let loaderTimeout;
+
+function showLoaderWithTimeout() {
   document.querySelector('.loader-overlay').style.display = 'flex';
 }
 
+export function showLoader() {
+  // showing loader only if the request takes more than a second, otherwise it looks cringe
+  loaderTimeout = setTimeout(showLoaderWithTimeout, 500);
+}
+
 export function hideLoader() {
+  clearTimeout(loaderTimeout);
   document.querySelector('.loader-overlay').style.display = 'none';
 }
